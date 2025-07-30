48c518efb1700e7b267c83b362e414f267126c2c
scudo <96631443+scudo005@users.noreply.github.com.>
2025-07-30 08:39:18 +0200

    js

--- assets/js/playAudioOnClick.js
+++ assets/js/playAudioOnClick.js
@@ -4,9 +4,9 @@ var buttons = document.getElementsByTagName('button');
 
 sound_play.play();
 
-if (navigator.getAutoplayPolicy(sound_play) != 'allowed') {
+/*if (navigator.getAutoplayPolicy(sound_play) != 'allowed') {
     alert('Please enable autoplay to hear navigation sounds.');
-}
+}*/
 
 // magical function: I spent like an hour making this. LOL
 for (var i = 0; i < buttons.length; i++) {
