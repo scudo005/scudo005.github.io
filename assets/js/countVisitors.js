c0a08e992f5b61b173322a2d695a920e539eae9c
scudo <96631443+scudo005@users.noreply.github.com.>
2025-07-30 08:39:20 +0200

    added site hit code

--- assets/js/countVisitors.js
+++ assets/js/countVisitors.js
@@ -2,7 +2,8 @@ req = new XMLHttpRequest();
 req.open("GET", "https://scudo-count.bruschetta.cc/count");
 req.setRequestHeader("Accept", "application/json");
 req.onload = () => {
-    if (req.readyState == 4 && req.status == 201) {
+    if (req.readyState === 4 && req.status === 201) {
+        // add this when the CORS problem is fixed.
         //document.getElementById('main').appendChild(document.createElement('p').appendChild(document.createTextNode(req.responseText + req.status)));
         console.log(JSON.parse(req.responseText));
         console.log(req.status);
