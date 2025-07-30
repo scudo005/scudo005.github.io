16908fbc6733dec3a7a66a23188f8b9faeb813d0
scudo <96631443+scudo005@users.noreply.github.com.>
2025-07-30 08:39:19 +0200

    perf & bugfixes

--- assets/js/playAudioOnClick.js
+++ assets/js/playAudioOnClick.js
@@ -1,28 +1,9 @@
 var links = Array.from(document.querySelectorAll('a'));
 var sound_play = document.getElementById('nav_button');
 var buttons = document.getElementsByTagName('button');
-// stupid fucking browsers auto loop it??????
 sound_play.loop = false;
-
 addEventListener("DOMContentLoaded", (event) => {
-    // this check works only on Firefox; on Chromium, it just plays the sound.
-    if (navigator.getAutoplayPolicy(sound_play) === 'allowed') {
-        sound_play.play();
-    }
-    // we need this so the page does not get stuck infinitely loading sometimes
-    click();
-});
-
-if (navigator.userAgent.includes("Mozilla/5.0 (Android")) {
-    var domBody = document.getElementById('main');
-    var warnBox = document.createElement('p');
-    var warnTxt = document.createTextNode('Warning: this site is currently broken under Firefox for Android, and I do not know why. For an optimal experience on mobile, please use a Chromium-based browser.');
-    warnBox.appendChild(warnTxt);
-    domBody.appendChild(warnBox);
-}
-
-function click() {
-    // magical function: I spent like an hour making this. LOL
+    sound_play.play();
     for (var i = 0; i < buttons.length; i++) {
         (function (i) {
             if (buttons.item(i).className === 'button_link') {
@@ -32,4 +13,4 @@ function click() {
             }
         }(i));
     }
-}
+});
