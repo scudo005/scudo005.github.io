6970d948d03447c2ed0cd0471253e65614b4dd00
scudo <96631443+scudo005@users.noreply.github.com.>
2025-08-04 11:27:01 +0200

    test

--- assets/js/countVisitors.js
+++ assets/js/countVisitors.js
@@ -1,7 +1,7 @@
 req = new XMLHttpRequest();
 req.open("GET", "https://scudo-count.bruschetta.cc/count");
-req.setRequestHeader("Accept", "application/json, text/plain");
-req.onload = () => {
+req.setRequestHeader("Accept", "application/json"); // server always has this header
+req.onload = () => { // checking server and request status
     if (req.status != 200) {
         console.log('Invalid request status ' + req.status);
     }
@@ -9,9 +9,14 @@ req.onload = () => {
         console.log('Request not completed correctly. Current state:  ' + req.readyState);
     }
 }
-req.send();
-console.log('server response: ' + req.response)
-if (req.responseText === ''){
-    document.getElementById('main').appendChild(document.createElement('p').appendChild(document.createTextNode('Something went wrong. See the browser console for details.')));
+req.send(); // sending get request
+req.addEventListener('loadend', show); // wait for it to finish loading
+function show(){
+    console.log('server response: ' + req.response)
+    if (req.response === ''){
+            document.getElementById('main').appendChild(document.createElement('p').appendChild(document.createTextNode('Something went wrong. See the browser console for details.')));
+    }
+    document.getElementById('main').appendChild(document.createElement('p').appendChild(document.createTextNode(req.response)));
 }
-document.getElementById('main').appendChild(document.createElement('p').appendChild(document.createTextNode(req.responseText)));
+
+
