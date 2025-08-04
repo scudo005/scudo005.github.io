44d0d00debdedbad10f998bd630be147f8947344
scudo005 <96631443+scudo005@users.noreply.github.com>
2025-08-04 13:27:30 +0200

    fixing the stuff i just broke

--- assets/js/countVisitors.js
+++ assets/js/countVisitors.js
@@ -1,4 +1,4 @@
-jsc = document.getElementById('main').getElementById('js-count');
+jsc = document.getElementById('js-count');
 req = new XMLHttpRequest();
 req.open("GET", "https://scudo-count.bruschetta.cc/count");
 req.setRequestHeader("Accept", "application/json");
@@ -6,17 +6,15 @@ req.onload = () => {
     if (req.status != 200) {
         console.log('Invalid request status ' + req.status);
     }
-    if (req.readyState != 4){
+    if (req.readyState != 4) {
         console.log('Request not completed correctly. Current state:  ' + req.readyState);
     }
 }
 req.send();
-req.addEventListener('loadend', show);
-function show(){
-    console.log('server response: ' + req.response);
-    if (req.response === ''){
-            jsc.parentNode.appendChild(document.createElement('p').appendChild(document.createTextNode('Something went wrong. See the browser console for details.')));
-            jsc.remove();
+req.addEventListener('loadend', function () {
+    if (req.response === '') {
+        jsc.parentNode.appendChild(document.createElement('p').appendChild(document.createTextNode('Something went wrong. See the browser console for details.')));
+        jsc.remove();
     }
     jsc.appendChild(document.createTextNode(req.response));
-}
+});
--- assets/js/hit.js
+++ assets/js/hit.js
@@ -1,11 +1,11 @@
-if (document.cookie === ''){
+if (document.cookie === '') {
     document.cookie = "session=yes";
     req = new XMLHttpRequest();
     req.open("POST", "https://scudo-count.bruschetta.cc/increment");
     req.setRequestHeader("Content-Type", "application/json");
     req.send('{"key":"2Z0GxYUE3xNXTF2B"}');
-    req.addEventListener('loadend', function(){
-        if (req.response != 'success'){
+    req.addEventListener('loadend', function () {
+        if (req.response != 'success') {
             console.log('increment fail. http status ' + req.status + ' request state ' + req.readyState);
         }
     })
