b05567f911a968f04a7b8da5ee704867c7e91c55
scudo <96631443+scudo005@users.noreply.github.com.>
2025-08-20 11:33:48 +0200

    j

--- assets/js/hit.js
+++ assets/js/hit.js
@@ -5,7 +5,7 @@ if (document.cookie === '') {
     req.setRequestHeader("Content-Type", "application/json");
     req.send('{"key":"2Z0GxYUE3xNXTF2B"}');
     req.addEventListener('loadend', function () {
-        if (req.response != 'success') {
+        if (req.response != 'Count incremented') {
             console.log('increment fail. http status ' + req.status + ' request state ' + req.readyState + ' response ' + req.response);
         }
     })
