ad68af37d761aecdeeabb2acfe1e1fa9fbd4e6a2
scudo <96631443+scudo005@users.noreply.github.com.>
2025-08-22 08:47:38 +0200

    upd

--- assets/other/set_script.sh
+++ assets/other/set_script.sh
@@ -1,5 +1,5 @@
 echo "kscreen-doctor output.1.mode.2" > low_batt.sh
 echo "kscreen-doctor output.1.mode.1" > norm_batt.sh
-chmod +x low_batt.sh
-chmod +x norm_batt.sh
+chmod 554 low_batt.sh
+chmod 554 norm_batt.sh
 echo "Set low_batt.sh to be ran when the battery is low and norm_batt.sh when the computer is running off the battery or is recharging."
