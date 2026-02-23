9a703848d5a348d3aaa42720552d4483a5698f0e
scudo <96631443+scudo005@users.noreply.github.com>
2026-02-23 16:14:34 +0100

    xmpp

--- assets/js/email.js
+++
@@ -1,6 +0,0 @@
-const link = document.createElement('a');
-const node = document.createTextNode('here');
-link.appendChild(node);
-link.title = 'here';
-link.href = 'mailto:scudo005@outlook.com';
-document.getElementById('mail').appendChild(link);
